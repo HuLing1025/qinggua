fa3f81faa1394afa70c94c37130f3edf360fda36
HuLing <huling1025@youloft.com>
2020-12-21 16:57:43 +0800

    feat : init project

---
+++ src/main/java/com/yznu/qinggua/QingguaApplication.java
@@ -0,0 +1,13 @@
+package com.yznu.qinggua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingguaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yznu/qinggua/QingguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yznu.qinggua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingguaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
