41b762b4ffdebf538fbd3bcdaa8ff3e5227a6aa7
HuLing <huling1025@youloft.com>
2021-01-08 11:05:49 +0800

    fix:MD5两次加密

--- src/main/java/com/yznu/qinggua/utils/MD5.java
+++ src/main/java/com/yznu/qinggua/utils/MD5.java
@@ -4,7 +4,7 @@ import org.springframework.util.DigestUtils;
 
 public class MD5 {
     public static String getMD5(String code) {
-        // MD5加密
-        return DigestUtils.md5DigestAsHex(code.getBytes());
+        // MD52次加密
+        return DigestUtils.md5DigestAsHex(DigestUtils.md5DigestAsHex(code.getBytes()).getBytes());
     }
 }
