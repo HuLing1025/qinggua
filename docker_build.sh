73b9a5a62902d73c4f42ebcab9688b5e2d39cb65
HuLing <huling1025@youloft.com>
2021-03-30 16:21:00 +0800

    fix:切换docker仓库

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
 # build the latest image
-docker build -t huling1025/qinggua:latest .
+docker build -t zhangshixue328/qinggua:latest .
 # login
-docker login  --username=huling1025 --password Huling1025
+docker login  --username=zhangshixue328 --password zhangshixue
 # push the latest image
-docker push huling1025/qinggua:latest
+docker push zhangshixue328/qinggua:latest
 # delete the image
-docker rmi huling1025/qinggua:latest
+docker rmi zhangshixue328/qinggua:latest
--- docker_reploy.sh
+++ docker_reploy.sh
@@ -3,10 +3,10 @@ docker stop qinggua_prod
 # delete the container
 docker rm qinggua_prod
 # delete the image
-docker rmi huling1025/qinggua:latest
+docker rmi zhangshixue328/qinggua:latest
 # login
-docker login  --username=huling1025 --password Huling1025
+docker login  --username=zhangshixue328 --password zhangshixue
 # pull the latest image
-docker pull huling1025/qinggua:latest
+docker pull zhangshixue328/qinggua:latest
 # run the latest image and mount the images folder
-docker run -v /images/:/images/ -v /var/log/docker:/var/log -p 8088:8088 --name qinggua_prod -d huling1025/qinggua:latest
+docker run -v /images/:/images/ -v /var/log/docker:/var/log -p 8088:8088 --name qinggua_prod -d zhangshixue328/qinggua:latest
